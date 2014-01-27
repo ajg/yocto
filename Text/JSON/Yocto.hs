f19c6812fce1050d140b1a9358649123e8efa76a
Alvaro J. Genial <genial@alva.ro>
2014-01-26 19:51:42 -0500

    Remove need for (>>>)

--- Text/JSON/Yocto.hs
+++ Text/JSON/Yocto.hs
@@ -32,27 +32,26 @@ syntax = whitespace >> value where
   whitespace = many (oneOf " \t\r\n")
   value = null <|> string <|> array <|> object <|> number <|> boolean
 
-  null    = Null    <$ keyword "null"
+  null    = Null    <$  keyword "null"
+  boolean = Boolean <$> (True <$ keyword "true" <|> False <$ keyword "false")
+  number  = Number  <$> rational <$> lexical (integer & fraction & exponent)
   string  = String  <$> many character       `enclosedBy` (char,  '"', '"')
   array   = Array   <$> commaSeparated value `enclosedBy` (token, '[', ']')
   object  = Object  <$> commaSeparated pair  `enclosedBy` (token, '{', '}')
-  number  = Number  <$> rational <$> lexical (integer & fraction & exponent)
-  boolean = Boolean <$> (keyword "true"  >>> True
-                    <|> (keyword "false" >>> False))
 
   pair = name & (token ':' >> value) where name = string =>> \(String s) -> s
   character = satisfy (\c -> not (isControl c) && c /= '"' && c /= '\\')
           <|> (char '\\' >> ((char 'u' >> count 4 hexDigit =>> ordinal)
                         <|> oneOf "\"\\/bfnrt"))
 
-  integer  = (char '0' >>> 0 <|> natural) `maybeSignedWith` minus =>> (% 1)
+  integer  = (0 <$ char '0' <|> natural) `maybeSignedWith` minus =>> (% 1)
   fraction = option 0 (char '.' >> many1 digit =>> fractional)
   exponent = option 0 (oneOf "eE" >> natural `maybeSignedWith` (plus <|> minus))
 
   commaSeparated = (`sepBy` token ',')
   items `enclosedBy` (term, start, end) = term start *> items <* term end
   it `maybeSignedWith` sign = (option (+) sign =>> ($ 0)) <*> it
-  (plus, minus) = (char '+' >>> (+), char '-' >>> (-))
+  (plus, minus) = ((+) <$ char '+', (-) <$ char '-')
 
   lexical  = (<* whitespace)
   integral = fst . head . readDec
@@ -69,12 +68,11 @@ syntax = whitespace >> value where
 
 instance Show Value where
   show  Null           = "null"
-  show (Boolean True)  = "true"
-  show (Boolean False) = "false"
+  show (Boolean b)     = if b then "true" else "false"
+  show (Number r)      = showRational r
   show (String chars)  = "\"" ++ concat (showChar <$> chars) ++ "\""
   show (Array values)  = "[" ++ intercalate "," (show <$> values) ++ "]"
   show (Object pairs)  = "{" ++ intercalate "," (showPair <$> pairs) ++ "}"
-  show (Number r)      = showRational r
 
 showPair (name, value) = show name ++ ":" ++ show value
 showChar c = maybe control (\e -> "\\" ++ [e]) (c `lookup` exceptions)
@@ -88,4 +86,4 @@ showRational r | remainder == 0 = show whole
   where (whole, remainder) = (numerator r) `divMod` (denominator r)
 
 -- Give more intuitive names to the Functor combinators:
-a & b = (,) <$> a <*> b; a =>> b = b <$> a; a >>> b = b <$ a
+a & b = (,) <$> a <*> b; a =>> b = b <$> a
