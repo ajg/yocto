f21d42fd53a44381ae43734cd6ec5a3917d3c93f
Alvaro J. Genial <genial@alva.ro>
2014-02-09 03:15:46 -0500

    Derive Arbitrary instance for Value manually

--- Tests.hs
+++ Tests.hs
@@ -6,9 +6,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
-import Data.Derive.All
-import Data.Derive.Arbitrary
-import Data.DeriveTH
+import Control.Applicative
+import Control.Monad
 import Data.Map
 import Data.Ratio
 import Test.QuickCheck
@@ -17,19 +16,41 @@ import Test.QuickCheck.Arbitrary
 import Test.QuickCheck.Instances
 import Text.JSON.Yocto
 
+{-
+-- NOTE: This doesn't work because it either (a) enters infinite recursion or
+         (b) creates ridiculously large data sets that never terminate in a
+         practical memory space or time scale. So we have to create the
+         instance manually (see below.)
 
-instance Arbitrary (Ratio Integer) where
-  arbitrary = do
-    d <- arbitrary :: Gen Integer
-    return $ fromInteger d
-    -- return (1 % 2)
-    {- d <- arbitrary :: Gen Double
-    return $ (toRational d :: Rational) -}
-
-
--- derive makeFunctor ''Value
+import Data.Derive.All
+import Data.Derive.Arbitrary
+import Data.DeriveTH
 
 derive makeArbitrary ''Value
+-}
+
+instance Arbitrary Value where
+  arbitrary = sized value
+
+value 0 = return Null
+value n | n > 0 = oneof [
+    Boolean <$> arbitrary,
+    Number  <$> arbitrary,
+    String  <$> arbitrary,
+    Array   <$> replicateM m subvalue,
+    Object  <$> fromList <$> replicateM m pair
+  ]
+  where subvalue = value m
+        pair = (,) <$> arbitrary <*> subvalue
+        m = n `div` 2
+
+-- Limit rationals to those with finite decimal expansions.
+-- TODO: Figure out a more intelligent way to do this.
+instance Arbitrary Rational where
+  arbitrary = oneof [
+    (% 1) <$> arbitrary,
+    (% 2) <$> arbitrary,
+    (% 5) <$> arbitrary]
 
 prop_identity v = decode (encode v) == v
 
